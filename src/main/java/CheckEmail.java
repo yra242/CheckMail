7ce0e6ad61460176e73a4986c971f5652b87518b
Вікторія <vikaban@gmail.com>
2023-03-15 16:29:39 +0200

    Check Email Test

---
+++ src/main/java/CheckEmail.java
@@ -0,0 +1,13 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CheckEmail {
+    public boolean checkEmail(String text) {
+
+        String regex = "^[a-zA-Z][a-zA-Z0-9.]*[a-zA-Z0-9]@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        return matcher.matches();
+
+    }
+}
---
+++ src/test/java/CheckEmailTest.java
@@ -0,0 +1,44 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CheckEmailTest {
+    private static CheckEmail checkEmail;
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        System.out.println("@BeforeClass setUpBeforeClass()");
+        checkEmail = new CheckEmail();
+    }
+
+    @Test
+    public void testEmail1() {
+        System.out.println("testEmail1");
+        Assert.assertTrue(checkEmail.checkEmail("vikaban@gmail.com"));
+    }
+
+    @Test
+    public void testEmail2() {
+        System.out.println("testEmail2");
+        Assert.assertTrue(checkEmail.checkEmail("vikaban2@gmail.com"));
+    }
+
+    @Test
+    public void testEmail3() {
+        System.out.println("testEmail3");
+        Assert.assertFalse(checkEmail.checkEmail("vikaban.@gmail.com"));
+    }
+
+
+    @Test
+    public void testEmail4() {
+        System.out.println("testEmail4");
+        Assert.assertFalse(checkEmail.checkEmail("vikaban.@g"));
+    }
+
+    @Test
+    public void testEmail5() {
+        System.out.println("testEmail5");
+        Assert.assertFalse(checkEmail.checkEmail("vikaban.g"));
+    }
+}
